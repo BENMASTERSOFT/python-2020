0936af3ad9f592836e315480994e27252cd90b14
BENMASTERSOFT <consultmastersoft@gmail.com>
2020-08-28 20:42:31 +0100

    Numbers and strings

--- strings.py
+++ strings.py
@@ -1,5 +1,25 @@
 import os
 os.system('clear')
 
-greetings = 'My Boss Yelled "Get Back to Work"'
+name = "Ben Mastersoft"
+
+#Concantenation
+greetings = "hello, my name is " + name
 print(greetings)
+
+#Change of Cases
+print(greetings.upper())
+print(greetings.lower())
+print(greetings.capitalize())
+print(greetings.title())
+print(greetings.swapcase())
+
+#length of a String
+print(len(greetings))
+print(greetings[13])
+print(greetings[18:21])
+
+#String Splitting
+res = greetings.split(" ")
+print(res)
+print(res[4:6])
