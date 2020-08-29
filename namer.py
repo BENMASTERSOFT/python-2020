c3246b7ada3f6b8723bb9d1eccbe06c2c3a80a57
Benjamin <consultmastersoft@gmail.com>
2020-08-29 19:03:52 +0100

    All Files

--- namer.py
+++
@@ -1,8 +0,0 @@
-#####################################
-# Learn Python Coding in 2020
-# By Ben Mastersoft
-# mastersoft.com.ng
-###################################
-
-def nameit(name):
-	print("Hello there " + name)
