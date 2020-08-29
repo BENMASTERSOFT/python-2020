f3187019a5376571743bc2360b0728832ee39991
Benjamin <consultmastersoft@gmail.com>
2020-08-29 18:48:04 +0100

    SQLite

---
+++ funct1.py
@@ -0,0 +1,11 @@
+#####################################
+# Learn Python Coding in 2020
+# By Ben Mastersoft
+# mastersoft.com.ng
+###################################
+import os
+from namer import nameit
+
+os.system("clear")
+
+nameit("Ben")
---
+++ namer.py
@@ -0,0 +1,8 @@
+#####################################
+# Learn Python Coding in 2020
+# By Ben Mastersoft
+# mastersoft.com.ng
+###################################
+
+def nameit(name):
+	print("Hello there " + name)
